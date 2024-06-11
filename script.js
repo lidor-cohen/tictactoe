76812db5f11c9fa4fe9d8d6f8da9fbc6704fcba3
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-06-12 00:19:15 +0300

    Added cell functionality, added a status player turn

--- script.js
+++ script.js
@@ -3,18 +3,35 @@ const Gameboard = (() => {
 
   this.init = () => {
     this.cacheDom();
+    this.bindEvents();
     this.render();
   };
 
   this.cacheDom = () => {
     this.boardWrapper = document.querySelector(".board-wrapper");
     this.boardItems = document.querySelectorAll(".cell");
+    this.statusText = document.querySelector("#status-text");
+  };
+
+  this.bindEvents = () => {
+    this.boardItems.forEach((element) => {
+      element.addEventListener("click", clickCell, false);
+    });
+  };
+
+  this.clickCell = (e) => {
+    statusText.textContent = `${GameController.getPlayerTurn().sign}'s Turn`;
+    let sign = GameController.changeTurn().sign;
+    e.srcElement.textContent = sign;
+    e.srcElement.removeEventListener("click", clickCell);
   };
 
   this.render = () => {
     boardItems.forEach((data, index) => {
       data.textContent = gameboard[index];
     });
+
+    statusText.textContent = "X's Turn";
   };
 
   return { init };
@@ -33,12 +50,24 @@ const GameController = (() => {
   this.changeTurn = () => {
     if (turn === 1) {
       turn = 2;
+      console.log(this.player2);
+      return this.player2;
     } else {
       turn = 1;
+      console.log(this.player1);
+      return this.player1;
     }
-
-    return { changeTurn };
   };
+
+  this.getPlayerTurn = () => {
+    if (turn === 1) {
+      return this.player1;
+    } else {
+      return this.player2;
+    }
+  };
+
+  return { changeTurn, getPlayerTurn };
 })();
 
 Gameboard.init();
